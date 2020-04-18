63fb620ed7fca08c7af1fd6ea9d438f9fd642acc
panda8z <panda8@126.com>
2020-04-19 00:05:34 +0800

    leetcode  & timepill api

---
+++ 2020-04-17/demo01/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func addTwo(nums []int, target int) []int {
+	m := map[int]int{}
+	for i, v := range nums {
+		if j, ok := m[target-v]; ok {
+			return []int{i, j}
+		}
+		m[v] = i
+	}
+	return nil
+}
+
+func main() {
+	fmt.Println(addTwo([]int{1, 2, 3, 4, 5, 6}, 7))
+	fmt.Println(addTwo([]int{1, 2, 3, 4, 5, 6}, 9))
+}
