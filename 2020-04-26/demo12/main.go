f4d05f7d40534faf2e8b2fd7bd9e0500b3b81e17
panda8z <panda8@126.com>
2020-04-29 00:11:36 +0800

    add

---
+++ 2020-04-26/demo12/main.go
@@ -0,0 +1,36 @@
+// Qiut Channel
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func boring(msg string, quit chan bool) <-chan string {
+	c := make(chan string)
+	go func() {
+		for i := 0; ; i++ {
+			select {
+			case c <- fmt.Sprintf("%s: %d", msg, i):
+				// do nothing
+			case <-quit:
+				fmt.Println("Oh! You Stop Me!")
+				return
+			}
+
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+		}
+	}()
+	return c
+}
+
+func main() {
+	quit := make(chan bool)
+	c := boring("Joe", quit)
+	for i := rand.Intn(10); i >= 0; i-- {
+		fmt.Println(<-c)
+	}
+	quit <- true
+}
