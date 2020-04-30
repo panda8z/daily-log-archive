5b21305c4a9f873f537cbdbfbc9d381dd8b55012
panda8z <panda8@126.com>
2020-04-30 16:56:52 +0800

    增加Google IO 2012 go concurrency的整理

---
+++ 2020-04-26/google-search-01/main.go
@@ -0,0 +1,48 @@
+// Google Search 1.0
+// Synchronous
+// The Google function takes a query and returns a slice of Results(which are just strings)
+// Google invokes Web, Image and Video searches serially, appending them to the results slice.
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var (
+	web   = fakeSearch("web")
+	image = fakeSearch("image")
+	video = fakeSearch("video")
+)
+
+// Result type
+type Result string
+
+// Search fake
+type Search func(query string) Result
+
+func fakeSearch(kind string) Search {
+	return func(query string) Result {
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+		return Result(fmt.Sprintf("%s result for %q\n", kind, query))
+	}
+}
+
+// Google ,
+func Google(query string) (results []Result) {
+	results = append(results, web(query))
+	results = append(results, image(query))
+	results = append(results, video(query))
+	return results
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	start := time.Now()
+	result := Google("google")
+	elapsed := time.Since(start)
+	fmt.Println(result)
+	fmt.Println(elapsed)
+
+}
---
+++ 2020-04-26/google-search-02/main.go
@@ -0,0 +1,59 @@
+// Google Search 2.0
+// Run the Web, Image and Video searches concurrently, and wait for all results.
+// No locks, No condition variables, No callbacks
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var (
+	web   = fakeSearch("web")
+	image = fakeSearch("image")
+	video = fakeSearch("video")
+)
+
+// Result type
+type Result string
+
+// Search fake
+type Search func(query string) Result
+
+func fakeSearch(kind string) Search {
+	return func(query string) Result {
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+		return Result(fmt.Sprintf("%s result for %q\n", kind, query))
+	}
+}
+
+// Google ,
+func Google(query string) (results []Result) {
+	c := make(chan Result)
+	go func() {
+		c <- web(query)
+	}()
+	go func() {
+		c <- image(query)
+	}()
+	go func() {
+		c <- video(query)
+	}()
+
+	for i := 0; i < 3; i++ {
+		result := <-c
+		results = append(results, result)
+	}
+	return results
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	start := time.Now()
+	result := Google("google")
+	elapsed := time.Since(start)
+	fmt.Println(result)
+	fmt.Println(elapsed)
+
+}
