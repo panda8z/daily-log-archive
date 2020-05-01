cc1c5986c3b8dc20fd056d698bb039e53a9f7823
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-01 09:40:51 +0800

    Deploy panda8z/daily-log-archive to github.com/panda8z/daily-log-archive.git:gh-pages

---
+++ 2020-04-26/google-search-00/main.go
@@ -0,0 +1,41 @@
+// Google Search: A fake framework
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var (
+	web   = fakeSearch("web")
+	image = fakeSearch("image")
+	video = fakeSearch("video")
+)
+
+// Result type
+type Result string
+
+// Search fake
+type Search func(query string) Result
+
+func fakeSearch(kind string) Search {
+	return func(query string) Result {
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+		return Result(fmt.Sprintf("%s result for %q\n", kind, query))
+	}
+}
+
+// Google ,
+func Google(query string) Result {
+	return Result(fmt.Sprintf("%s%s%s", web(query), image(query), video(query)))
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	start := time.Now()
+	result := Google("google")
+	elapsed := time.Since(start)
+	fmt.Println(result, elapsed)
+
+}
