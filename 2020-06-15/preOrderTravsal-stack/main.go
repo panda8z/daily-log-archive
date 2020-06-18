51a222ea9442106af7ba093fdcdc5cd85ad94ff3
panda8z <panda8@126.com>
2020-06-18 22:48:36 +0800

    add some code

--- 2020-06-15/preOrderTravsal-stack/main.go
+++ 2020-06-15/preOrderTravsal-stack/main.go
@@ -23,13 +23,14 @@ func (s *Stack) Push(t *TreeNode) {
 func (s *Stack) Pop() *TreeNode {
 	n := []*TreeNode(*s)[len(*s)-1]
 	*s = []*TreeNode(*s)[:len(*s)-1]
+	return n
 }
 
 func preorderTraversal(root *TreeNode) []int {
 	if root == nil {
 		return []int{}
 	}
-	stack, r := []*TreeNode{root}, []int{}
+	stack, r := Stack([]*TreeNode{root}), []int{}
 	for len(stack) > 0 {
 		cur := stack.Pop()
 		if cur != nil {
@@ -50,6 +51,42 @@ func preorderTraversal(root *TreeNode) []int {
 	return r
 }
 func main() {
+	var m int8 = -5
+	fmt.Printf("-5 uint8 %b\n", uint8(m))
+
+	
+	 i := -2147483648
+	 fmt.Printf("i type is  : %T\n",i)
+	 fmt.Printf("i binary is: %032b\n", i)
+	 var a uint64 
+	 a = uint64(i)
+	 fmt.Printf("a type is  : %T\n",a)
+	 fmt.Printf("a value is : %v\n",a)
+	 fmt.Printf("a binary is: %064b\n", a)
+
+
+	fmt.Printf("1 << 31    : %#b\n", 1<<31)
+	fmt.Printf("-(1 << 31) : %#032b\n", -(1 << 31))
+	/*
+
+	   1 << 31 : 2147483648
+	   1 << 31 : 2147483648
+	   1 << 31 : 1000 0000 0000 0000 0000 0000 0000 0000
+
+
+	   1000 0000 0000 0000 0000 0000 0000 0000
+	   1111 1111 1111 1111 1111 1111 1111 1111
+	 1 0000 0000 0000 0000 0000 0000 0000 0000
+
+	 1111111111111111111111111111111110000000000000000000000000000000
+	 -1
+	 1111 1111 1111 1111 1111 1111 1111 1111 0111 1111 1111 1111 1111 1111 1111 1111
+	 取反
+	 0000 0000 0000 0000 0000 0000 0000 0000 1000 0000 0000 0000 0000 0000 0000 0000
+	*/
+}
+
+func testTreeNodePreoderTraversal() {
 	treeT := &TreeNode{
 		Val: 3,
 		Left: &TreeNode{
@@ -74,10 +111,9 @@ func main() {
 
 	r := preorderTraversal(treeT)
 	fmt.Println(r)
-
 }
 
-func test() {
+func testSliceExpressions() {
 	a := [5]int{1, 2, 3, 4, 5}
 	s := a[1:4]
 	fmt.Println(s)
