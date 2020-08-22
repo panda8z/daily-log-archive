2456386552577e9f118ce2ad6faa66cf69d5bed0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-22 08:21:52 +0800

    Deploy panda8z/daily-log-archive to github.com/panda8z/daily-log-archive.git:gh-pages

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    title: 'Panda8z`Blog',
+    description: 'Panda8z developer engineer designer',
+    port: 8011,
+    themeConfig: {
+        logo: 'https://golang.google.cn/lib/godoc/images/go-logo-blue.svg',
+        // nav: [
+        //     { text: 'Home', link: '/' },
+        //     { text: '2020-08-18', link: '/2020-08-18/' },
+        // ],
+        // sidebar: [
+        //     ['./2020-08-18/Cmake教程01-入门和第一个项目.md', "Cmake教程01-入门和第一个项目"],
+        //     ['./2020-08-18/Cmake教程02-配置版本号.md', "Cmake教程02-配置版本号"],
+        //     ['./2020-08-18/Cmake教程03-指定C++11编译标准.md', "Cmake教程03-指定C++11编译标准"],
+        //     ['./2020-08-18/Cmake教程04-生成lib或so库.md', "Cmake教程04-生成lib或so库"],
+        //     ['./2020-08-18/Cmake教程05-install打包.md', "Cmake教程05-install打包"],
+        // ]
+
+    },
+}
