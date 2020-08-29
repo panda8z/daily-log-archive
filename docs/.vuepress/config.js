623364e54aea13fe5fd2da91196180ff7d41f704
panda8z <panda8@126.com>
2020-08-30 00:15:16 +0800

    add

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,17 +4,43 @@ module.exports = {
     port: 8011,
     themeConfig: {
         logo: 'https://golang.google.cn/lib/godoc/images/go-logo-blue.svg',
-        // nav: [
-        //     { text: 'Home', link: '/' },
-        //     { text: '2020-08-18', link: '/2020-08-18/' },
-        // ],
-        // sidebar: [
-        //     ['./2020-08-18/Cmake教程01-入门和第一个项目.md', "Cmake教程01-入门和第一个项目"],
-        //     ['./2020-08-18/Cmake教程02-配置版本号.md', "Cmake教程02-配置版本号"],
-        //     ['./2020-08-18/Cmake教程03-指定C++11编译标准.md', "Cmake教程03-指定C++11编译标准"],
-        //     ['./2020-08-18/Cmake教程04-生成lib或so库.md', "Cmake教程04-生成lib或so库"],
-        //     ['./2020-08-18/Cmake教程05-install打包.md', "Cmake教程05-install打包"],
-        // ]
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'About', link: '/about.md' },
+            {
+                text: '分类',
+                ariaLabel: 'Kinds',
+                items: [
+                    { text: 'Rust', link: '/rust/', target: 'self' },
+                    { text: 'CMake', link: '/cmake/', target: 'self' },
+                    { text: '技术面试', link: '/技术面试/', target: 'self' },
+                ]
+            },
+
+        ],
+        sidebar: {
+            '/cmake/': [
+                ['./Cmake教程01-入门和第一个项目.md', 'Cmake教程01-入门和第一个项目'],
+                ['./Cmake教程02-配置版本号.md', 'Cmake教程02-配置版本号'],
+                ['./Cmake教程03-指定C++11编译标准.md', 'Cmake教程03-指定C++11编译标准'],
+                ['./Cmake教程04-生成lib或so库.md', 'Cmake教程04-生成lib或so库'],
+                ['./Cmake教程05-install打包.md', 'Cmake教程05-install打包'],
+            ],
+            '/技术面试/': [
+                ['./字节面试01-后端.md', '字节面试01-后端'],
+                ['./字节面试02-java后端已入职.md', '字节面试02-java后端已入职'],
+                ['./字节面试03-后端.md', '字节面试03-后端'],
+                ['./字节面试04-后端失败了.md', '字节面试04-后端失败了'],
+                ['./字节面试05-后端算法题.md', '字节面试05-后端算法题'],
+                ['./技术面试刷题指南.md', '技术面试刷题指南'],
+            ],
+            '/rust/': [
+                ['./2020-08-28-rust-day01-笔记.md', 'rust-day01-笔记']
+            ],
+            '/': [
+                './about.md'
+            ],
+        }
 
     },
 }
